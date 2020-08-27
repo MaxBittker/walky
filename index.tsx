2e06fb6564827effe8719de51c9e930c60427af2
max bittker <maxbittker@gmail.com>
2020-08-26 22:23:55 -0400

    build

--- index.tsx
+++ index.tsx
@@ -1,119 +1,119 @@
 import * as React from "react";
+import ReactDOM from "react-dom";
 import * as ReactDOMServer from "react-dom/server";
 import walk from "./assets/walk1.gif";
 import stand from "./assets/stand1.gif";
+import * as Matter from "matter-js";
+let Vector = Matter.Vector;
 
-let imageHTML = ReactDOMServer.renderToStaticMarkup(
-  <img id="walker" src={walk}></img>
-);
-let wd = document.getElementById("window");
+// let imageHTML = ReactDOMServer.renderToStaticMarkup(
+//   <img id="walker" src={walk}></img>
+// );
+// let wd = document.getElementById("window");
 
-wd.innerHTML = imageHTML;
-let walker = document.getElementById("walker");
+// wd.innerHTML = imageHTML;
+// let walker = document.getElementById("walker");
 
-let x = 200;
-let y = 200;
-let vx = 0;
-let vy = 0;
+let pos: Matter.Vector = { x: 200, y: 200 };
+let camera: Matter.Vector = { x: 200, y: 200 };
+let velocity: Matter.Vector = { x: 0, y: 0 };
 
-let targetX: ?Number = null;
-let targetY: ?Number = null;
+let target: Matter.Vector = null;
 
 window.addEventListener("click", event => {
-  targetX = event.pageX;
-  targetY = event.pageY;
+  target = { x: event.pageX, y: event.pageY };
 });
 
-
 window.addEventListener("touchmove", event => {
-
   // event. preventDefault();
   const touches = event.targetTouches;
 
   for (let i = 0; i < touches.length; i++) {
-    targetX = touches[i].pageX;
-    targetY = touches[i].pageY;
-  
+    target = { x: touches[i].pageX, y: touches[i].pageY };
   }
-
 });
 
 window.addEventListener("touchstart", event => {
-
   // event. preventDefault();
   const touches = event.targetTouches;
 
   for (let i = 0; i < touches.length; i++) {
-    targetX = touches[i].pageX;
-    targetY = touches[i].pageY;
-  
+    target = { x: touches[i].pageX, y: touches[i].pageY };
   }
-
 });
 
-let speed = 2
+let speed = 2;
 window.addEventListener("keydown", event => {
-  targetX = null;
-  targetY = null;
+  target = null;
   if (event.keyCode === 37) {
-    vx = -speed;
+    velocity.x = -speed;
   }
   if (event.keyCode === 38) {
-    vy = -speed;
+    velocity.y = -speed;
   }
   if (event.keyCode === 39) {
-    vx = speed;
+    velocity.x = speed;
   }
   if (event.keyCode === 40) {
-    vy = speed;
+    velocity.y = speed;
   }
 });
+
+function render() {
+  let moving = false;
+  if (Math.abs(velocity.x) > 0.01 || Math.abs(velocity.y) > 0.01) {
+    moving = true;
+  }
+  let newsrc = moving ? walk : stand;
+
+  const element = (
+    <React.Fragment>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <img
+        id="walker"
+        src={newsrc}
+        style={{
+          left: pos.x,
+          top: pos.y,
+          transform: `translate(-50%, -50%) scaleX(${velocity.x < 0 ? -1 : 1})`
+        }}
+      ></img>
+    </React.Fragment>
+  );
+  ReactDOM.render(element, document.getElementById("window"));
+}
 window.addEventListener("keyup", event => {
   if (event.keyCode === 37) {
-    vx = -0.000000001;
+    velocity.x = -0.000000001;
   }
   if (event.keyCode === 38) {
-    vy = -0;
+    velocity.y = -0;
   }
   if (event.keyCode === 39) {
-    vx = 0;
+    velocity.x = 0;
   }
   if (event.keyCode === 40) {
-    vy = 0;
+    velocity.y = 0;
   }
 });
-let wasMoving = false;
 function tick() {
-  if (targetX != null) {
-    if( Math.abs(targetX-x)>10){
-      vx = speed * (targetX < x ? -1 : 1);
-    }else{
-      vx = 0;
+  if (target != null) {
+    if (Math.abs(target.x - pos.x) > 10) {
+      velocity.x = speed * (target.x < pos.x ? -1 : 1);
+    } else {
+      velocity.x = 0;
     }
-    if( Math.abs(targetY-y)>10 ){
-     vy = speed * (targetY < y ? -1 : 1);
-    }else{
-      vy = 0
+    if (Math.abs(target.y - pos.y) > 10) {
+      velocity.y = speed * (target.y < pos.y ? -1 : 1);
+    } else {
+      velocity.y = 0;
     }
   }
 
-  x += vx;
-  y += vy;
+  pos = Vector.add(pos, velocity);
 
-  let moving = false;
-  if(Math.abs(vx)>0.01 || Math.abs(vy)>0.01){
-    moving = true
-  }
-  console.log(moving);
-  walker.style = `left: ${x}; top: ${y};   transform: translate(-50%, -50%) scaleX(${
-    vx < 0 ? -1 : 1
-  });`;
-  let newsrc = moving ? walk : stand;
-  if(wasMoving!== moving){
-  
-      walker.src = newsrc;
-      wasMoving = moving;
-  }
+  render();
+
   window.requestAnimationFrame(tick);
 }
 
