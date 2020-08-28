d9fa7fef8e829e67d2e473208dc07b92a99bd41c
max bittker <maxbittker@gmail.com>
2020-08-27 23:39:36 -0400

    build

--- src/input.ts
+++ src/input.ts
@@ -44,36 +44,62 @@ function startInput() {
     }
   });
 
-  // window.addEventListener("keydown", event => {
-  //   target = null;
+  let left = false;
+  let up = false;
+  let right = false;
+  let down = false;
 
-  //   if (event.keyCode === 37) {
-  //     velocity.x = -speed;
-  //   }
-  //   if (event.keyCode === 38) {
-  //     velocity.y = -speed;
-  //   }
-  //   if (event.keyCode === 39) {
-  //     velocity.x = speed;
-  //   }
-  //   if (event.keyCode === 40) {
-  //     velocity.y = speed;
-  //   }
-  // });
-  // window.addEventListener("keyup", event => {
-  //   if (event.keyCode === 37) {
-  //     velocity.x = -0;
-  //   }
-  //   if (event.keyCode === 38) {
-  //     velocity.y = -0;
-  //   }
-  //   if (event.keyCode === 39) {
-  //     velocity.x = 0;
-  //   }
-  //   if (event.keyCode === 40) {
-  //     velocity.y = 0;
-  //   }
-  // });
+  window.addEventListener("keydown", event => {
+    if (event.keyCode === 37) {
+      left = true;
+    }
+    if (event.keyCode === 38) {
+      up = true;
+    }
+    if (event.keyCode === 39) {
+      right = true;
+    }
+    if (event.keyCode === 40) {
+      down = true;
+    }
+  });
+  window.addEventListener("keyup", event => {
+    if (event.keyCode === 37) {
+      left = false;
+    }
+    if (event.keyCode === 38) {
+      up = false;
+    }
+    if (event.keyCode === 39) {
+      right = false;
+    }
+    if (event.keyCode === 40) {
+      down = false;
+    }
+  });
+  function pollKeys() {
+    if (!(left || right || up || down)) {
+      return;
+    }
+    let velocity = { x: 0, y: 0 };
+    if (left) {
+      velocity.x -= 1.0;
+    }
+    if (right) {
+      velocity.x += 1.0;
+    }
+    if (up) {
+      velocity.y -= 1.0;
+    }
+    if (down) {
+      velocity.y += 1.0;
+    }
+
+    let state = getState();
+    let keyTarget = Vector.add(state.me.pos, Vector.mult(velocity, 5));
+    state.me.target = keyTarget;
+  }
+  window.setInterval(pollKeys, 16);
 }
 
 export { startInput };
