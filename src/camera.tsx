1ced4c6ec6df49ed7d81f9d43816fa6c413a330f
max bittker <maxbittker@gmail.com>
2020-09-01 00:04:50 -0400

    build

--- src/camera.tsx
+++ src/camera.tsx
@@ -2,22 +2,27 @@ import * as Matter from "matter-js";
 import { getState } from "./state";
 import { speed } from "./movement";
 let Vector = Matter.Vector;
-
+let zoom = window.innerWidth <= 600 ? 0.6 : 0.0;
+window.onresize = () => {
+  zoom = window.innerWidth <= 600 ? 0.6 : 0.0;
+};
 function updateCamera(elapsedTicks: number) {
   let state = getState();
-  const { me, camera, frame } = state;
+  const { me, camera, frame, center } = state;
   let { pos } = me;
-  let distanceFromPos = Vector.magnitude(Vector.sub(pos, camera));
 
+  // unfortunately this line makes no sense to me:
+  pos = Vector.sub(pos, Vector.mult(center, zoom));
+
+  let distanceFromPos = Vector.magnitude(Vector.sub(pos, camera));
   let camera_speed = speed;
   if (distanceFromPos < Vector.magnitude(frame) / 20) {
     camera_speed = 0;
   }
   camera_speed *= distanceFromPos / (Vector.magnitude(frame) / 6);
   let directionTowardsPos = Vector.normalise(Vector.sub(pos, camera));
-
   camera_speed *= elapsedTicks;
-  // state.camera = { x: 0, y: 0 };
+
   state.camera = Vector.add(
     camera,
     Vector.mult(directionTowardsPos, camera_speed)
