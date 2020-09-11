7a8da48e10ae48df44976c0d75cfb54a590ca7a0
max bittker <maxbittker@gmail.com>
2020-09-11 12:05:48 -0400

    build

--- src/ui.tsx
+++ src/ui.tsx
@@ -170,6 +170,7 @@ class UI extends React.Component {
           className="tool"
           id="chat-image"
           onClick={e => {
+            e.stopPropagation();
             let state = getState();
             state.me.target = undefined;
             fakeInput.focus();
